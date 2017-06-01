379df587d23da98e16912f1bc8f4f31a76be7e83
曹國鴻 <371645070@qq.com>
2017-06-01 21:53:44 +0800

    Update mian.js

--- static/js/mian.js
+++ static/js/mian.js
@@ -1,19 +1,24 @@
+/**
+ * Created by apple on 2017/6/1.
+ */
+// 获取数据
 var uploadedDataURL = "/graph";
-// var uploadedDataURL = "/static/data.new.json";
+// 基于准备好的dom，初始化echarts实例
+// var uploadedDataURL = "http://gallery.echartsjs.com/asset/get/s/data-1495595908173-rk2KsOfWb.json";
 
 $.getJSON(uploadedDataURL, function(linedata) {
-    var data = linedata[0]
-    var links = linedata[1]
-    var categories = linedata[2]
-    var cont = linedata[3]
-    var mid = linedata[4]
-    var user1 = linedata[5]
+    var data = linedata[0];
+    var links = linedata[1];
+    var categories = linedata[2];
+    var cont = linedata[3];
+    var mid = linedata[4];
+    var user1 = linedata[5];
     option = {
 
         title: {
-            text: "微博转发关系图",
-            subtext: '@' + user1 + ':' + cont,
-            sublink: 'http://m.weibo.cn/status/' + mid,
+            text: "雪梨教育用户关系数据系统",
+            subtext: '@土豆豆',
+            sublink: mid,
             top: "top",
             left: "center"
         },
@@ -38,15 +43,15 @@ $.getJSON(uploadedDataURL, function(linedata) {
         animationDuration: 1500,
         animationEasingUpdate: 'quinticInOut',
         series: [{
-            name: '微博',
+            name: '雪梨教育',
             type: 'graph',
             layout: 'force',
 
             force: {
                 //initLayout:'circular'
                 edgeLength: 50,
-                repulsion: 50,
-                gravity: 0.2
+                repulsion: 100,
+                gravity: 0.9
             },
             data: data,
             edges: links,
@@ -61,7 +66,7 @@ $.getJSON(uploadedDataURL, function(linedata) {
             },
             lineStyle: {
                 normal: {
-                    color: 'target',
+                    //color: 'target',
                     curveness: 0
                 }
             }
